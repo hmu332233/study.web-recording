6d83c77fcd50aca67680c92deb9cefc5c07452e6
minung.han <hmu332233@gmail.com>
2021-08-14 20:59:53 +0900

    녹화 후 preview 예제

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # study.web-recording
 
-1. 카메라로 부터 입력 받기
-  - `MediaDevie.getUserMedia`
-  - https://developer.mozilla.org/ko/docs/Web/API/Media_Streams_API
-2. 카메라로 받은 입력을 HTMLVideoElement를 통해서 실시간으로 출력
-3. 카메라로 부터 받은 입력을 녹화
-4. 녹화된 영상 미리보기
-5. 녹화된 영상 다운로드
+Recording Process
+1. media data의 source 생성 (MediaStream or HTMLMediaElement)
+2. MediaRecorder 객체 생성 (source와 option 인수 전달)
+3. MediaRecorder.ondataavailable 이벤트 핸들러 등록
+4. source에서 데이터가 생성되면, MediaRecorder.start() 메소드를 호출하여 Recording 시작
+5. 매번 데이터가 준비될 때 마다 dataavailable 이벤트가 발생
+6. MediaRecorder.stop() 메소드를 호출하여 Recording 중지
 ### 참고
-- [링크](https://medium.com/watcha/%EC%9B%B9%EB%B8%8C%EB%9D%BC%EC%9A%B0%EC%A0%80%EC%97%90%EC%84%9C-%EB%8F%99%EC%9E%91%ED%95%98%EB%8A%94-%EB%85%B9%ED%99%94%EC%95%B1-%EB%A7%8C%EB%93%A4%EA%B8%B0-70142ce28994)
+- [링크1](https://melius.tistory.com/59)
+- [링크2](https://medium.com/watcha/%EC%9B%B9%EB%B8%8C%EB%9D%BC%EC%9A%B0%EC%A0%80%EC%97%90%EC%84%9C-%EB%8F%99%EC%9E%91%ED%95%98%EB%8A%94-%EB%85%B9%ED%99%94%EC%95%B1-%EB%A7%8C%EB%93%A4%EA%B8%B0-70142ce28994)
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const videoPreview = document.getElementById('video-preview');
 const videoOutput = document.getElementById('video-output');
+const startBtn = document.getElementById('start-btn');
+const finishBtn = document.getElementById('finish-btn');
+
+let mediaStream = null;
+let mediaRecorder = null;
+let recordedMediaUrl = null;
 
 // 1. 카메라로 부터 입력 받기
 const constraints = { audio: false, video: true };
@@ -6,14 +13,67 @@ navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
 	// 비디오 트랙을 포함한 MediaStream
 	console.log(mediaStream);
 
-  // 2. 카메라로 받은 입력을 HTMLVideoElement를 통해서 실시간으로 출력
-	// MediaStream을 HTMLVideoElement의 srouce로 설정
+	// 2. 카메라로 받은 입력을 HTMLVideoElement를 통해서 실시간으로 출력
+	setPreview(mediaStream);
+
+	// MediaStream을 통해 Record 이벤트 등록
+	setRecorder(mediaStream);
+});
+
+function setPreview(mediaStream) {
+	// MediaStream을 HTMLVideoElement의 source 설정
 	videoOutput.srcObject = mediaStream;
 	// metadata가 로드될 때 실행되는 이벤트
 	videoOutput.onloadedmetadata = function() {
 		// HTMLVideoElement로 카메라의 화면을 출력하기 시작
 		videoOutput.play();
 	};
-});
+}
 
+function setRecorder(stream) {
+	if (!MediaRecorder.isTypeSupported('audio/webm;codecs=opus')) {
+		alert('지원하지 않음');
+	}
 
+	// 1.MediaStream을 매개변수로 MediaRecorder 생성자를 호출
+	const options = {
+		audioBitsPerSecond: 128000,
+		mimeType: 'audio/webm;codecs=opus'
+	};
+	const mediaRecorder = new MediaRecorder(stream, options);
+
+	// 2. 전달받는 데이터를 처리하는 이벤트 핸들러 등록
+	// 매번 데이터가 준비될 때 마다 dataavailable 이벤트가 발생
+	const recordedChunks = [];
+	mediaRecorder.addEventListener('dataavailable', function(e) {
+		if (e.data.size > 0) {
+			recordedChunks.push(e.data);
+		}
+	});
+
+	mediaRecorder.addEventListener('stop', function() {
+		// 생성된 Blob을 매개변수로 URL.createObjectURL 메서드를 호출하면 URL 생성
+		// url 사용 완료 이후에는 revokeObjectURL을 호출해줘야함 (메모리 누수 방지)
+
+		// audio 예제
+		// const blob = new Blob(recordedChunks);
+		// const url = window.URL.createObjectURL(blob);
+		// const audio = document.querySelector('audio');
+		// audio.src = url;
+
+		// video 예제
+		const blob = new Blob(recordedChunks, { 'type': 'video/mp4' });
+    const url = window.URL.createObjectURL(blob);
+		videoPreview.src = url;
+
+    console.log(url);
+	});
+
+	// 녹화 시작, 녹화 종료 핸들러 등록
+	startBtn.onclick = () => {
+		mediaRecorder.start();
+	};
+	finishBtn.onclick = () => {
+		mediaRecorder.stop();
+	};
+}
